dfdb247218d0c578867e63a832abbbd3714224a3
ishkul10008-debug <ishkul10008@gmail.com>
2026-08-17 18:00:11 +0200

    Add files via upload

--- muliplication_table_generator.py
+++ muliplication_table_generator.py
@@ -1,32 +1,7 @@
-import tkinter as tk
-from tkinter.ttk import*
-gui=tk.Tk()
+from tkinter import*
+gui=Tk()
 #gui.geometry('300x300')
-title=tk.Label(gui, text='multiplication table generator ',bg='white', font=('Arial', 14, 'bold'))
+title=Label(gui, text='multiplication table generator ',bg='white', font=('Arial', 14, 'bold'))
 title.grid(row=0,column=2 ,columnspan=3)
-numrange=tk.Label(gui, text=' Select the number and the range',bg='white',font=('Arial',8,'bold'))
-def result():
-    store=''
-    for i in range(radio.get()):
-         num.get()*i
-         print(num.get()*(i+1))
-         store=store+num.get()*(i+1)
-multiplication=tk.Label(gui )
-multiplication.grid(row =7,column=5)
-result1=tk.Button(gui, text='Result',bg='white',font=('Arial',8,'bold'),command=result)
-result1.grid(row=5,column=3)
-numrange.grid(row=1,column=3)
-num=tk.IntVar()
-comboxstorage=Combobox(gui,textvariable=num)
-comboxstorage.grid(row=1,column=4)
-comboxstorage['values']=tuple(range(101))
-radio=tk.IntVar()
-radiostorage1=Radiobutton(gui,text='10',variable=radio,value=10)
-radiostorage1.grid(row=2,column=5)
-radiostorage2=Radiobutton(gui,text='20',variable=radio,value=20)
-radiostorage2.grid(row=4,column=5)
-radiostorage3=Radiobutton(gui,text='30',variable=radio,value=30)
-radiostorage3.grid(row=6,column=5)
-
 
 gui.mainloop()
